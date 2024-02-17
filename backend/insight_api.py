b57328c6a2dd752aaeb13e2af161c671126aa180
Jacoba91 <jaragao91@icloud.com>
2024-02-17 16:10:18 -0500

    Updated language model to 'gpt-4-turbo-preview' to minimize outdated team feedback.

--- backend/insight_api.py
+++ backend/insight_api.py
@@ -15,11 +15,11 @@ def get_openai_response(player_names):
     player_names_string = ', '.join(player_names)
 
     response = client.chat.completions.create(
-    model="gpt-3.5-turbo",
+    model="gpt-4-turbo-preview",
     messages=[
         {"role": "system", "content": "You are a helpful assistant for a fantasy american football league."},
         {"role": "user", "content": player_names_string},
-        {"role": "user", "content": "1. Strengths and Weaknesses Analysis: Examine the team's performance from a fantasy sports perspective. Identify and briefly explain two key strengths and two key weaknesses of the team. 2. Handcuff Options: Evaluate the roster in terms of the 'handcuffing' strategy. Suggest potential player pickups that could be beneficial and name a player who could be dropped to accommodate these new additions. 3. Based on a detailed statistical analysis of the team's roster, performance history, and potential in a full PPR league, assign a percentage grade (0-100%). The grade should reflect a comprehensive assessment, considering the team's depth, injury history, player consistency, and overall scoring potential. Please ensure the grade varies according to the specific characteristics and statistical strengths and weaknesses of each team."},
+        {"role": "user", "content": "1. Strengths and Weaknesses Analysis: Examine the team's performance from a fantasy sports perspective. Identify and briefly explain two key strengths and two key weaknesses of the team. 2. Handcuff Options: Evaluate the roster in terms of the 'handcuffing' strategy with up-to-date 2024 rosters. Suggest potential player pickups that could be beneficial and name a player who could be dropped to accommodate these new additions. 3. Based on a detailed statistical analysis of the team's roster, performance history, and potential in a full PPR league, assign a percentage grade (0-100%). The grade should reflect a comprehensive assessment, considering the team's depth, injury history, player consistency, and overall scoring potential. Please ensure the grade varies according to the specific characteristics and statistical strengths and weaknesses of each team."},
     ]
     )
 
