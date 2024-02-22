dca76de78574d3731f8a6510d95fcefab979c206
Jacoba91 <jaragao91@icloud.com>
2024-02-21 21:41:24 -0500

    Removed unnecessary files

--- backend/sqltester.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, jsonify
-from app import get_data  # Import get_data function
-
-app = Flask(__name__)
-
-with app.app_context():
-    data = get_data()
-    json_data = jsonify(data)
-    print(json_data.get_json())  # This should print the JSON data
